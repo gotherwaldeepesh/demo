5051887ad3e3f7be60f431729f4c8d1552a92c15
gotherwaldeepesh <22f1000099@ds.study.iitm.ac.in>
2024-04-30 22:11:31 +0530

    third commit

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def my_func():
     return "<h1> Hello world </h1>"
 
+@app.route('/home')
+def my_func2():
+    return "<h1> Hello home </h1>"
+
+
 if __name__ == "__main__":
     app.run()
 
